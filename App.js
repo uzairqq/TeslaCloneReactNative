4cf51b77cb52b560c0fc8827aba868f34d469dc8
Uzair Iqbal <uzair.qq@outlook.com>
2021-09-09 18:53:17 +0500

    Car Item Component Styling and Text only

--- App.js
+++ App.js
@@ -5,7 +5,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+
+      <View style={styles.carContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subTitle}>Starting From $69,460</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +26,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  carContainer: {
+    width: '100%',
+    height: '100%'
+  },
+  titles: {
+    marginTop: '40%',
+    alignItems: 'center',
+    width: '100%'
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '500'
+  },
+  subTitle: {
+    fontSize: 16,
+    color: '#5c5e62'
+
+  }
 });
