4adc4685e6a655893d4f7686daca54ffa6953659
Uzair Iqbal <uzair.qq@outlook.com>
2021-09-09 19:25:24 +0500

    Render And Adding Background Image With Styling

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
@@ -8,6 +8,11 @@ export default function App() {
 
 
       <View style={styles.carContainer}>
+
+        <ImageBackground
+          source={require('./assets/images/ModelS.jpeg')}
+          style={styles.backgroundImage}
+        />
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subTitle}>Starting From $69,460</Text>
@@ -43,6 +48,11 @@ const styles = StyleSheet.create({
   subTitle: {
     fontSize: 16,
     color: '#5c5e62'
-
+  },
+  backgroundImage: {
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+    resizeMode: 'cover'
   }
 });
