4521b39e03ca52993da31c9303cc9ac681ccf46e
Uzair Iqbal <uzair.qq@outlook.com>
2021-09-13 23:23:51 +0500

    Empty Button Component

---
+++ components/styledButton/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const StyledButton = (props) => {
+    return (
+        <View>
+            <Text>Syled Button Component</Text>
+        </View>
+    )
+}
+
+export default StyledButton
