d53fb0d31b4f550ef4a91e51824f555b923bc179
Uzair Iqbal <uzair.qq@outlook.com>
2022-05-31 15:52:51 +0500

    Render the text, View Etc with their styles

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subTitle}>Starting at Price $69,450</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  carContainer: {
+    height: "100%",
+    width: "100%",
+  },
+  titles: {
+    marginTop: "30%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  subTitle: {
+    fontSize: 16,
+    color: "grey",
   },
 });
