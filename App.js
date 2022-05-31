9cbf4627cbaf14ef57319a764e2b471ff045d894
Uzair Iqbal <uzair.qq@outlook.com>
2022-05-31 17:09:30 +0500

    Background Image With Styling

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.carContainer}>
+        <ImageBackground
+          source={require("./assets/images/ModelS.jpeg")}
+          style={styles.image}
+        />
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subTitle}>Starting at Price $69,450</Text>
@@ -38,5 +42,11 @@ const styles = StyleSheet.create({
   subTitle: {
     fontSize: 16,
     color: "grey",
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    resizeMode: "contain",
+    position: "absolute",
   },
 });
